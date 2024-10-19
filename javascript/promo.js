1a398780a0f72682c6f62a2223a239d9903a23a7
Sohorab Mahmud Fahim <sohorab.ndc.edu@gmail.com>
2024-10-19 20:07:27 +0600

    Delete javascript directory

--- javascript/promo.js
+++
@@ -1,6 +0,0 @@
-function playVideo() {
-    const video = document.getElementById('promo-video-element');
-    video.play();
-    video.style.zIndex = "5";
-    document.querySelector('.play-button').style.display = 'none';
-}
